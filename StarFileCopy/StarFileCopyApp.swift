d94aed43220c162a61016721f6b0e683b5e4973f
alankiy <alankiy@me.com>
2026-04-02 18:49:15 +0900

    Initial Commit

---
+++ StarFileCopy/StarFileCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarFileCopyApp.swift
+//  StarFileCopy
+//
+//  Created by AlanKiy on 2026/04/02.
+//
+
+import SwiftUI
+
+@main
+struct StarFileCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
